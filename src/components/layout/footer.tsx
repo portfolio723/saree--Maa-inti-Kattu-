824274616671d7582c74ad9a4a755533874032fa
Sandeep <agencysandesigns@gmail.com>
2025-10-30 11:12:16 +0000

    We Accept image in the footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -74,7 +74,7 @@ export function Footer() {
                 <div>
                    <h3 className="font-semibold font-headline mb-2 relative after:content-[''] after:absolute after:left-0 after:bottom-[-4px] after:w-10 after:h-0.5 after:bg-primary">We Accept</h3>
                    <div className="flex items-center gap-2 mt-4">
-                     <Image src="https://kalamandir.com/pub/media/porto/paypal.png" alt="Paypal" width={48} height={30} className="object-contain" />
+                     <Image src="https://kalamandir.com/pub/media/porto/paypal.png" alt="Paypal" width={96} height={60} className="object-contain" />
                    </div>
                 </div>
              </div>
